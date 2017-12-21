f8baca985ec437b1ab1122946bc96f0bce78882b
Philly Kelly <p_a_k_@hotmail.com>
2017-12-21 23:03:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 
 function forLoop(array) {
-  for (let beans = 0; beans < 25; i++) {
-    if beans === 1 {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
       array.push("I am ${i} strange loop")
     } else {
       array.push("I am ${i} strange loops")
